d55df672340a4a2b9dc556fdbcdf569892bcfd9b
Bhutesh  kumar meher <98634733+Bhuteshkumarmeher07@users.noreply.github.com>
2023-12-25 12:53:28 +0530

    Create 91. Decode Ways.cpp

---
+++ 91. Decode Ways.cpp
@@ -0,0 +1,20 @@
+    int numDecodings(string s) {
+        int n = s.length();
+        vector<int> dp(n+1, 0);
+
+        dp[0] = 1; // Empty string
+        dp[1] = (s[0] == '0') ? 0:1; // first char
+
+        for(int i = 2; i<= n; i++)
+        {
+            if(s[i-1] != '0'){
+                dp[i] += dp[i-1];
+            }
+
+            int num = stoi(s.substr(i-2, 2));
+            if(s[i-2] != '0' && num <= 26){
+                dp[i] += dp[i-2];
+            }
+        }
+        return dp[n];
+    }
