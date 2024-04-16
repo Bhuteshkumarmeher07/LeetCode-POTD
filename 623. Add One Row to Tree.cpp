c5357b74df1a475b17f77ee2e3f0e07ec47714e8
Bhutesh  kumar meher <98634733+Bhuteshkumarmeher07@users.noreply.github.com>
2024-04-16 19:26:31 +0530

    Create 623. Add One Row to Tree.cpp

---
+++ 623. Add One Row to Tree.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
+        if(depth == 1) {
+            TreeNode* new_node = new TreeNode(val);
+            new_node->left = root;
+            return new_node;
+        }
+
+        queue<TreeNode*> pq;
+        pq.push(root);
+        int level = 0;
+        bool flag = false;
+
+        while(!pq.empty()) {
+            int n = pq.size();
+            level += 1;
+            if(flag) break;
+
+            for(int i = 0; i<n; i++) {
+                TreeNode* curr = pq.front();
+                pq.pop();
+
+                if(level == depth - 1) {
+                    flag = true;
+                    
+                    TreeNode* new_node1 = new TreeNode(val);
+                    new_node1->left = curr->left;
+                    curr->left = new_node1;
+
+                    TreeNode* new_node2 = new TreeNode(val);
+                    new_node2->right = curr->right;
+                    curr->right = new_node2;
+                }
+                else {
+                    if(curr->left) pq.push(curr->left);
+                    if(curr->right) pq.push(curr->right);
+                }
+            }
+        }
+
+        return root;
+    }
+};
