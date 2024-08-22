1f33c1124e9e91c675a13d5bbc13f89bd307a353
Bhutesh  kumar meher <98634733+Bhuteshkumarmeher07@users.noreply.github.com>
2024-08-22 12:52:53 +0530

    Create 476. Number Complement.cpp

---
+++ 476. Number Complement.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int numBits = (int) (log2(num)) + 1;
+
+        for(int i=0; i<numBits; i++){
+            num = num ^ (1 << i);
+        }
+        return num;
+    }
+    // TC = O(log2(num))
+};
